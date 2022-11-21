8942f7b9f6382ffd6a7aa0c7361fc5e40753d424
kittycoder123 <110904871+kittycoder123@users.noreply.github.com>
2022-11-21 15:52:13 +0530

    Add files via upload

---
+++ math quiz.py
@@ -0,0 +1,78 @@
+one=int(input("5x11="))
+if one ==55:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+two=int(input("50-10="))
+if two ==40:
+    print("good")
+else:
+    print("opps...try harder on the next queston")   
+three=int(input("12x3="))
+if three ==36:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+four=int(input("7x7="))
+if four ==49:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+five=int(input("17-17="))
+if five==0:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+six=int(input("44-3="))
+if six ==41:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+seven=int(input("90-12="))
+if seven==78:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+eight=int(input("101+100="))
+if eight==201:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+nine=int(input("122+5="))
+if nine ==127:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+ten=int(input("78-9="))
+if ten ==69:
+    print("good")
+else:
+    print("opps...try harder on the next queston") 
+eleven=int(input("9+99="))
+if eleven ==108:
+    print("good")
+else:
+    print("opps...try harder on the next queston")  
+twelve=int(input("144-2="))
+if twelve==146:
+    print("good")
+else:
+    print("opps...try harder on the next queston") 
+thirteen=int(input("77+10="))
+if thirteen ==87:
+    print("good")
+else:
+    print("opps...try harder on the next queston")   
+fourteen=int(input("11x3="))
+if fourteen ==33:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+fifteen=int(input("7+6="))
+if fifteen ==13:
+    print("good")
+else:
+    print("opps...try harder on the next queston")    
+
+
+
